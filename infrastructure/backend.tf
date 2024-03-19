798dd25630624eaef30e90a2767d5469b8a5a310
Blaise Shu <61931171+snblaise@users.noreply.github.com>
2024-03-19 19:16:43 -0400

    Delete infrastructure directory

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
-    encrypt        = true
-  }
-}
